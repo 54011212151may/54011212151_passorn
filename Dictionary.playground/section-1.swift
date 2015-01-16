cfef05353464610766ee976675d197b5d41ac693
54011212151may <istudents@iClassrooms-iMac-3.local>
2015-01-16 17:02:38 +0700

    week1

---
+++ Dictionary.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+var airports: [String: String] = ["TYO": "Tokyo", "DUB": "Dublin"]
+if airports.isEmpty{
+    println("The airport dictionary is empty.")
+} else{
+    println("The airport dictionary is not empty.")
+}
+   // println "The airport dictionary is not empty."
+
+    //println("The airport dictionary contains \ (airport.count) items.")
+    //println"The airport dictionary contains 2 item"

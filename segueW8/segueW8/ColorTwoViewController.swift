717191314a285482f13191a530c47216bbf54198
54011212302daow <istudents@iClassrooms-iMac-3.local>
2015-03-06 17:21:05 +0700

    week8_Segue

---
+++ segueW8/segueW8/ColorTwoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorTwoViewController.swift
+//  segueW8
+//
+//  Created by iStudents on 3/6/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ColorTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
